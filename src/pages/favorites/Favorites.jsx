1f017d4b145c97a6cfadfac8525d819777e800bc
Mariano Silva <mariano.silva@dtidigital.com>
2023-07-11 23:36:43 -0300

    comecando pagina de favoritos

--- src/pages/favorites/Favorites.jsx
+++ src/pages/favorites/Favorites.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Banner from '../../Components/Banner/Banner'
 import Title from '../../Components/Title/Title'
+import styles from "./Favorites.module.css"
+import Card from '../../Components/Card/Card'
 
 const Favorites = () => {
   return (
     <>
         <Banner imageName="favoritos"/>
         <Title><h1>Meus Favoritos</h1></Title>
+        <section className={styles.container}>
+            <Card id="1" titulo="Gato Bonifacio" bannerImage="https://caelum-online-public.s3.amazonaws.com/2802-react-praticando/img4.png"/>
+        </section>
     </>
   )
 }
